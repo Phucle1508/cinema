30315bdaf3d8196fee11eb82191c74dadc35326c
Phucle1508 <lhphuc150809@gmail.com>
2025-05-05 20:23:56 +0700

    Carousel

--- script/index.js
+++ script/index.js
@@ -1,8 +1,15 @@
+import { API_KEY } from "./config.js";
 import {
   signOut,
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 import { auth } from "./config.js";
+
+// Để cho menu-toggle hoạt động khi click vào
+document.querySelector(".menu-toggle").addEventListener("click", () => {
+  const navContainer = document.querySelector(".nav-container");
+  navContainer.classList.toggle("active");
+});
 
 // Kiểm tra xem người dùng đã đăng nhập chưa
 function renderHeader() {
@@ -55,3 +62,145 @@ function renderHeader() {
   });
 }
 renderHeader();
+
+// Fetch trend movie
+const fetchTrending = async (timeWindow) => {
+  const url = `https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=${API_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw Error("Network was not ok", response.status);
+    }
+
+    const data = await response.json();
+    // console.log(data.results);
+    return data.results;
+  } catch (e) {
+    console.log("Error", e);
+  }
+};
+
+// CAROUSEL CINEMA
+async function initializeCarousel() {
+  try {
+    const movies = await fetchTrending("day");
+    const currentDate = new Date().getDate();
+
+    // Lấy 3 phim từ danh sách theo ngày hiện tại
+    const mainMovies = [
+      movies[currentDate % movies.length],
+      movies[(currentDate + 1) % movies.length],
+      movies[(currentDate + 2) % movies.length],
+    ];
+
+    mainMovies.forEach((movie, index) => {
+      if (index === 0) {
+        document.querySelector(".carousel1").innerHTML += `
+        <div class="hero-slide">
+          <img 
+            id="hero-image-${index}" 
+            src="https://image.tmdb.org/t/p/original${movie.backdrop_path}" 
+            alt="${movie.title}"
+            class="hero-backdrop"
+          />
+          <div class="hero-content">
+            <img 
+              id="hero-preview-image-${index}" 
+              src="https://image.tmdb.org/t/p/w300${movie.poster_path}" 
+              alt="${movie.title}"
+              class="hero-poster"
+            />
+            <div class="hero-info">
+              <h1 id="hero-title-${index}">${movie.title || movie.name}</h1>
+              <p id="hero-description-${index}">${movie.overview}</p>
+              <div class="hero-buttons">
+                <a href="#" class="btn btn-primary">
+                  <i class="fas fa-play"></i>
+                  <span>Xem ngay</span>
+                </a>
+                <a href="#" class="btn btn-secondary">
+                  <i class="fas fa-info-circle"></i>
+                  <span>Xem thông tin</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+      }
+      if (index === 1) {
+        document.querySelector(".carousel2").innerHTML += `
+          <div class="hero-slide">
+          <img 
+            id="hero-image-${index}" 
+            src="https://image.tmdb.org/t/p/original${movie.backdrop_path}" 
+            alt="${movie.title}"
+            class="hero-backdrop"
+          />
+          <div class="hero-content">
+            <img 
+              id="hero-preview-image-${index}" 
+              src="https://image.tmdb.org/t/p/w300${movie.poster_path}" 
+              alt="${movie.title}"
+              class="hero-poster"
+            />
+            <div class="hero-info">
+              <h1 id="hero-title-${index}">${movie.title || movie.name}</h1>
+              <p id="hero-description-${index}">${movie.overview}</p>
+              <div class="hero-buttons">
+                <a href="#" class="btn btn-primary">
+                  <i class="fas fa-play"></i>
+                  <span>Xem ngay</span>
+                </a>
+                <a href="#" class="btn btn-secondary">
+                  <i class="fas fa-info-circle"></i>
+                  <span>Xem thông tin</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+      }
+      if (index === 2) {
+        document.querySelector(".carousel3").innerHTML += `
+          <div class="hero-slide">
+          <img 
+            id="hero-image-${index}" 
+            src="https://image.tmdb.org/t/p/original${movie.backdrop_path}" 
+            alt="${movie.title}"
+            class="hero-backdrop"
+          />
+          <div class="hero-content">
+            <img 
+              id="hero-preview-image-${index}" 
+              src="https://image.tmdb.org/t/p/w300${movie.poster_path}" 
+              alt="${movie.title}"
+              class="hero-poster"
+            />
+            <div class="hero-info">
+              <h1 id="hero-title-${index}">${movie.title || movie.name}</h1>
+              <p id="hero-description-${index}">${movie.overview}</p>
+              <div class="hero-buttons">
+                <a href="#" class="btn btn-primary">
+                  <i class="fas fa-play"></i>
+                  <span>Xem ngay</span>
+                </a>
+                <a href="#" class="btn btn-secondary">
+                  <i class="fas fa-info-circle"></i>
+                  <span>Xem thông tin</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+      }
+    });
+  } catch (error) {
+    console.error("Error initializing carousel:", error);
+  }
+}
+
+initializeCarousel();
